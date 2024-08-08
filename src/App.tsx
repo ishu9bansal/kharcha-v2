9e7fec4617180a2d63229b65da8b28361c71645c
Ishu Bansal <ishubansal1400@gmail.com>
2024-08-08 19:17:38 +0530

    Fix ui issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import { AppBar, Tabs, Tab, Container } from '@mui/material';
 import ExpenseFormPage from './pages/ExpenseFormPage';
 import ExpenseListPage from './pages/ExpenseListPage';
@@ -16,9 +16,31 @@ function App() {
     <Router>
       <AppBar position="static">
         <Container>
-          <Tabs value={value} onChange={handleChange} centered>
-            <Tab label="Add Expense" component={Link} to="/" />
-            <Tab label="View Expenses" component={Link} to="/expenses" />
+        <Tabs value={value} onChange={handleChange} centered>
+            <Tab
+              label="Add Expense"
+              component={Link}
+              to="/"
+              sx={{
+                '&.Mui-selected': {
+                  backgroundColor: '#fff',
+                  color: '#1976d2',
+                  borderRadius: '8px 8px 0 0',
+                },
+              }}
+            />
+            <Tab
+              label="View Expenses"
+              component={Link}
+              to="/expenses"
+              sx={{
+                '&.Mui-selected': {
+                  backgroundColor: '#fff',
+                  color: '#1976d2',
+                  borderRadius: '8px 8px 0 0',
+                },
+              }}
+            />
           </Tabs>
         </Container>
       </AppBar>
