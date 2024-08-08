c518a2be0070c8048c9107541a511c11e745e23b
Ishu Bansal <ishubansal1400@gmail.com>
2024-08-08 15:19:45 +0530

    Material UI for the win!

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,168 +1,132 @@
-// src/components/DateInput.tsx
+// src/components/Inputs.tsx
 import React from 'react';
+import { TextField, FormControl, FormControlLabel, RadioGroup, Radio, Checkbox, Grid, Typography } from '@mui/material';
 
-interface DateInputProps {
-  value: string;
-  onChange: (value: string) => void;
-}
+export const DateInput: React.FC<{ value: string, onChange: (value: string) => void }> = ({ value, onChange }) => (
+  <TextField
+    label="Date"
+    type="date"
+    value={value}
+    onChange={(e) => onChange(e.target.value)}
+    fullWidth
+    InputLabelProps={{
+      shrink: true,
+    }}
+    required
+  />
+);
 
-export const DateInput: React.FC<DateInputProps> = ({ value, onChange }) => {
-  return (
-    <div>
-      <label>Date:</label>
-      <input type="date" value={value} onChange={(e) => onChange(e.target.value)} required />
-    </div>
-  );
-};
+export const AmountInput: React.FC<{ value: string, onChange: (value: string) => void }> = ({ value, onChange }) => (
+  <TextField
+    label="Amount"
+    type="number"
+    value={value}
+    onChange={(e) => onChange(e.target.value)}
+    fullWidth
+    required
+  />
+);
 
-// src/components/AmountInput.tsx
+export const TitleInput: React.FC<{ value: string, onChange: (value: string) => void }> = ({ value, onChange }) => (
+  <TextField
+    label="Title"
+    type="text"
+    value={value}
+    onChange={(e) => onChange(e.target.value)}
+    fullWidth
+    required
+  />
+);
 
-interface AmountInputProps {
-    value: string;
-    onChange: (value: string) => void;
-  }
-  
-export const AmountInput: React.FC<AmountInputProps> = ({ value, onChange }) => {
-  return (
-    <div>
-    <label>Amount:</label>
-    <input type="number" value={value} onChange={(e) => onChange(e.target.value)} required />
-    </div>
-  );
-};
-
-// src/components/TitleInput.tsx
-
-interface TitleInputProps {
-  value: string;
-  onChange: (value: string) => void;
-}
-
-export const TitleInput: React.FC<TitleInputProps> = ({ value, onChange }) => {
-  return (
-    <div>
-      <label>Title:</label>
-      <input type="text" value={value} onChange={(e) => onChange(e.target.value)} required />
-    </div>
-  );
-};
-
-// src/components/CategoryInput.tsx
-
-interface CategoryInputProps {
+export const CategoryInput: React.FC<{
   selectedCategory: string;
   onChange: (value: string) => void;
   newCategory: string;
   onNewCategoryChange: (value: string) => void;
-}
+}> = ({ selectedCategory, onChange, newCategory, onNewCategoryChange }) => {
+  const categories = [
+    "Entertainment", "Food", "Groceries", "Gift", "Apparel", "Self Care", "Donation", "Capital Expense", "Travel", "Repair", "Medical", "Miscellaneous", "Petrol"
+  ];
 
-const categories = [
-  "Entertainment", "Food", "Groceries", "Gift", "Apparel", "Self Care", "Donation", "Capital Expense", "Travel", "Repair", "Medical", "Miscellaneous", "Petrol"
-];
-
-export const CategoryInput: React.FC<CategoryInputProps> = ({ selectedCategory, onChange, newCategory, onNewCategoryChange }) => {
   return (
-    <div>
-      <label>Category:</label>
-      <div>
-        {categories.map(category => (
-          <label key={category}>
-            <input type="radio" name="category" value={category} checked={selectedCategory === category} onChange={() => onChange(category)} />
-            {category}
-          </label>
+    <FormControl component="fieldset" fullWidth>
+      <Typography component="legend">Category</Typography>
+      <RadioGroup
+        value={selectedCategory}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {categories.map((category) => (
+          <FormControlLabel
+            key={category}
+            value={category}
+            control={<Radio />}
+            label={category}
+          />
         ))}
-        <input type="text" value={newCategory} onChange={(e) => onNewCategoryChange(e.target.value)} placeholder="Add New Category" />
-      </div>
-    </div>
+        <TextField
+          label="Add New Category"
+          type="text"
+          value={newCategory}
+          onChange={(e) => onNewCategoryChange(e.target.value)}
+          fullWidth
+        />
+      </RadioGroup>
+    </FormControl>
   );
 };
 
-// src/components/PaymentModeInput.tsx
-
-interface PaymentModeInputProps {
+export const PaymentModeInput: React.FC<{
   selectedMode: 'Cash' | 'Digital';
   onChange: (value: 'Cash' | 'Digital') => void;
-}
+}> = ({ selectedMode, onChange }) => (
+  <FormControl component="fieldset" fullWidth>
+    <Typography component="legend">Payment Mode</Typography>
+    <RadioGroup
+      value={selectedMode}
+      onChange={(e) => onChange(e.target.value as 'Cash' | 'Digital')}
+    >
+      <FormControlLabel value="Cash" control={<Radio />} label="Cash" />
+      <FormControlLabel value="Digital" control={<Radio />} label="Digital" />
+    </RadioGroup>
+  </FormControl>
+);
 
-export const PaymentModeInput: React.FC<PaymentModeInputProps> = ({ selectedMode, onChange }) => {
-  return (
-    <div>
-      <label>Payment Mode:</label>
-      <div>
-        <label>
-          <input type="radio" name="payment-mode" value="Cash" checked={selectedMode === 'Cash'} onChange={() => onChange('Cash')} />
-          Cash
-        </label>
-        <label>
-          <input type="radio" name="payment-mode" value="Digital" checked={selectedMode === 'Digital'} onChange={() => onChange('Digital')} />
-          Digital
-        </label>
-      </div>
-    </div>
-  );
-};
+export const RecurringInput: React.FC<{ value: boolean, onChange: (value: boolean) => void }> = ({ value, onChange }) => (
+  <FormControlLabel
+    control={
+      <Checkbox
+        checked={value}
+        onChange={() => onChange(!value)}
+      />
+    }
+    label="Recurring"
+  />
+);
 
-// src/components/RecurringInput.tsx
-
-interface RecurringInputProps {
-  value: boolean;
-  onChange: (value: boolean) => void;
-}
-
-export const RecurringInput: React.FC<RecurringInputProps> = ({ value, onChange }) => {
-  return (
-    <div>
-      <label>Recurring:</label>
-      <input type="checkbox" checked={value} onChange={() => onChange(!value)} />
-    </div>
-  );
-};
-
-// src/components/BeneficiaryInput.tsx
-
-interface BeneficiaryInputProps {
+export const BeneficiaryInput: React.FC<{
   selectedBeneficiary: 'Self' | 'Family' | 'Friends' | 'Vehicle';
   onChange: (value: 'Self' | 'Family' | 'Friends' | 'Vehicle') => void;
-}
+}> = ({ selectedBeneficiary, onChange }) => (
+  <FormControl component="fieldset" fullWidth>
+    <Typography component="legend">Beneficiary</Typography>
+    <RadioGroup
+      value={selectedBeneficiary}
+      onChange={(e) => onChange(e.target.value as 'Self' | 'Family' | 'Friends' | 'Vehicle')}
+    >
+      <FormControlLabel value="Self" control={<Radio />} label="Self" />
+      <FormControlLabel value="Family" control={<Radio />} label="Family" />
+      <FormControlLabel value="Friends" control={<Radio />} label="Friends" />
+      <FormControlLabel value="Vehicle" control={<Radio />} label="Vehicle" />
+    </RadioGroup>
+  </FormControl>
+);
 
-export const BeneficiaryInput: React.FC<BeneficiaryInputProps> = ({ selectedBeneficiary, onChange }) => {
-  return (
-    <div>
-      <label>Beneficiary:</label>
-      <div>
-        <label>
-          <input type="radio" name="beneficiary" value="Self" checked={selectedBeneficiary === 'Self'} onChange={() => onChange('Self')} />
-          Self
-        </label>
-        <label>
-          <input type="radio" name="beneficiary" value="Family" checked={selectedBeneficiary === 'Family'} onChange={() => onChange('Family')} />
-          Family
-        </label>
-        <label>
-          <input type="radio" name="beneficiary" value="Friends" checked={selectedBeneficiary === 'Friends'} onChange={() => onChange('Friends')} />
-          Friends
-        </label>
-        <label>
-          <input type="radio" name="beneficiary" value="Vehicle" checked={selectedBeneficiary === 'Vehicle'} onChange={() => onChange('Vehicle')} />
-          Vehicle
-        </label>
-      </div>
-    </div>
-  );
-};
-
-// src/components/TagsInput.tsx
-
-interface TagsInputProps {
-  value: string;
-  onChange: (value: string) => void;
-}
-
-export const TagsInput: React.FC<TagsInputProps> = ({ value, onChange }) => {
-  return (
-    <div>
-      <label>Tags:</label>
-      <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
-    </div>
-  );
-};
+export const TagsInput: React.FC<{ value: string, onChange: (value: string) => void }> = ({ value, onChange }) => (
+  <TextField
+    label="Tags"
+    type="text"
+    value={value}
+    onChange={(e) => onChange(e.target.value)}
+    fullWidth
+  />
+);
